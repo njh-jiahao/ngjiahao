08d7300f04147aa41d9db9896190a7c6890e9e1c
Jia Hao Ng <kahou@Jias-MacBook-Pro.local>
2025-05-12 21:36:01 +0800

    metadata

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
+}
