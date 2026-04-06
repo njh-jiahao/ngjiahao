f70e2dede496d257941ebcb115a2638250e0c2a6
njh-jiahao <158319506+njh-jiahao@users.noreply.github.com>
2026-04-07 00:44:53 +0800

    Remove 'back-button' class from project link

--- app/project1/page.js
+++ app/project1/page.js
@@ -211,7 +211,7 @@ export default function Project1() {
             </div>
             <div className="project-cta-buttons">
               <Link href="/#contact" className="button">Get in touch →</Link>
-              <Link href="/#projects" className="button back-button">← Back to Projects</Link>
+              <Link href="/#projects" className="button">← Back to Projects</Link>
             </div>
           </div>
 
